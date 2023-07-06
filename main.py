48d1a44ccce2c73358a84033200cd14c21b91a5a
nekorobi_0 <65654766+nekorobi-0@users.noreply.github.com>
2023-07-07 02:45:10 +0900

    OpenCVで描画する

--- main.py
+++ main.py
@@ -8,8 +8,8 @@ import numpy as np
 from PIL import ImageFont, ImageDraw, Image
 showTL = True
 conf_dir = "conf.json"
-test = True
-
+test = False
+output = []
 def putText_jp(img, text, point, size, color):
     #https://monomonotech.jp/kurage/raspberrypi/opencv_japanese.html
     #Notoフォントとする
@@ -32,14 +32,23 @@ async def disp():
     while True:
         img = cv2.imread('background.png')
         cv2.line(img,(70,0),(70,160),(0,0,0))
+        #メニュー
         img = putText_jp(img, "Misskey Client \n   for Stormworks", (5, 5), 6, (0, 0, 0))
         img = putText_jp(img, "Home", (5, 25), 6, (0, 0, 0))
         img = putText_jp(img, "NOTE", (5, 35), 6, (0, 0, 0))
+        #TL
+        tl_text = ""
+        for i in output:
+            tl_text += f"   {i[0]}\n{i[1]}\n"
+        img = putText_jp(img, tl_text, (75,5), 6, (0, 0, 0))
         cv2.imshow('image', img)
-        cv2.waitKey(1)
+        cv2.waitKey(10)
+        await asyncio.sleep(0.01)
 
 async def getTL(token):
+    global output
     #TL取得
+    print("try to connect")
     uri = f"wss://stormskey.works/streaming?i={token}"
     async with websockets.connect(uri) as ws:
         await ws.send(json.dumps({
@@ -49,10 +58,13 @@ async def getTL(token):
 		        "id": '0',
             }
         }))
+        print("connected")
         while True:
             data = json.loads(await ws.recv())
-            output = str(data["body"]["body"]["user"]["name"])+":"+str(data["body"]["body"]["text"])
+            output.insert(0,[str(data["body"]["body"]["user"]["name"]),str(data["body"]["body"]["text"])])
             print(output)
+            if len(output) > 20:
+                output = output[:-1]
 
 def cv2stw(disp_array):
     #OpenCVで描画したのをストわにhttpで投げる
@@ -64,7 +76,7 @@ def getconf(file_dir):
     return json_load
 async def run():
     #非同期で描画処理とTL更新を同時にする
-    await asyncio.gather(disp(),getTL(conf["token"]))
+    await asyncio.gather(disp(),getTL(TOKEN))
 
 conf = getconf(conf_dir)
 TOKEN= conf["token"]
@@ -72,8 +84,9 @@ print(TOKEN)
 msk = Misskey('stormskey.works', i=TOKEN)
 MY_ID = msk.i()['id']
 WS_URL='wss://stormskey.works/streaming?i='+TOKEN
-if __name__ == "__main__" and not test:
+if __name__ == "__main__" and (not test):
     #asyncio.run(getTL(conf["token"]))
+    print("not Test mode")
     asyncio.run(run())
 elif __name__ =="__main__":
-    asyncio.run(disp())
+    asyncio.run(getTL(TOKEN))
