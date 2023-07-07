05acb6ec81b351769b41f19479cc93921c340aea
nekorobi_0 <65654766+nekorobi-0@users.noreply.github.com>
2023-07-08 00:13:28 +0900

    http鯖追加

--- main.py
+++ main.py
@@ -7,11 +7,13 @@ from misskey import Misskey
 import numpy as np
 from PIL import ImageFont, ImageDraw, Image
 import threading
+from http.server import BaseHTTPRequestHandler, HTTPServer
 
 showTL = True
 conf_dir = "conf.json"
 test = False
 output = []
+now_img = []
 
 class S(BaseHTTPRequestHandler):
 
@@ -24,12 +26,20 @@ class S(BaseHTTPRequestHandler):
 	def do_GET(self):
 
 		self._set_headers()
-		self.wfile.write("<html><body><h1>GET message receive!</h1></body></html>")
+		self.wfile.write(str(now_img).encode())
 
 	def do_POST(self):
 
 		self._set_headers()
-		self.wfile.write("<html><body><h1>POST message receive!</h1></body></html>")
+		self.wfile.write("<html><body><h1>POST message receive!</h1></body></html>".encode())
+
+def run_http_server(server_class=HTTPServer, handler_class=S, port=8888):
+
+	# startup HTTP server
+	server_address = ('', port)
+	httpd = server_class(server_address, handler_class)
+	print('HTTP server started....')
+	httpd.serve_forever()
 
 
 
@@ -52,6 +62,7 @@ def putText_jp(img, text, point, size, color):
 
 async def disp():
     #描画系
+    global now_img
     while True:
         img = cv2.imread('background.png')
         cv2.line(img,(70,0),(70,160),(0,0,0))
@@ -64,41 +75,52 @@ async def disp():
         for i in output:
             tl_text += f"   {i[0]}\n{i[1]}\n"
         img = putText_jp(img, tl_text, (75,5), 6, (0, 0, 0))
-        cv2.imshow('image', img)
-        cv2.waitKey(10)
+        img = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)#モノクロ
+        now_img = img.tolist()
+        show_img = cv2.resize(img, (1152,640))
+        cv2.imshow('image', show_img)
+        cv2.waitKey(1)
         await asyncio.sleep(0.01)
 
 async def getTL(token):
     global output
     #TL取得
-    print("try to connect")
-    uri = f"wss://stormskey.works/streaming?i={token}"
-    async with websockets.connect(uri) as ws:
-        await ws.send(json.dumps({
-	        "type": 'connect',
-	        "body": {
-		        "channel": 'localTimeline',
-		        "id": '0',
-            }
-        }))
-        print("connected")
-        while True:
-            data = json.loads(await ws.recv())
-            output.insert(0,[str(data["body"]["body"]["user"]["name"]),str(data["body"]["body"]["text"])])
-            print(output)
-            if len(output) > 20:
-                output = output[:-1]
-
+    while True:
+        print("try to connect")
+        uri = f"wss://stormskey.works/streaming?i={token}"
+        async with websockets.connect(uri) as ws:
+            await ws.send(json.dumps({
+	            "type": 'connect',
+	            "body": {
+		            "channel": 'localTimeline',
+		            "id": '0',
+                }
+            }))
+            print("connected")
+            while True:
+                try:
+                    data = json.loads(await ws.recv())
+                except websockets.exceptions.ConnectionClosedError:
+                    break
+                output.insert(0,[str(data["body"]["body"]["user"]["name"]),str(data["body"]["body"]["text"])])
+                print(output)
+                if len(output) > 20:
+                    output = output[:-1]
+        await asyncio.sleep(1)
 def cv2stw(disp_array):
-    #OpenCVで描画したのをストわにhttpで投げる
+    #OpenCVで描画したのをストわにhttpで投げるための処理
+    str
     return
 
 def getconf(file_dir):
     with open(file_dir,"r") as json_open:
         json_load = json.load(json_open)
     return json_load
+
 async def run():
-    #非同期で描画処理とTL更新を同時にする
+    #非同期で描画処理とTL更新とhttp鯖ホストを同時にする
+    thread1 = threading.Thread(target=run_http_server)#http鯖
+    thread1.start()
     await asyncio.gather(disp(),getTL(TOKEN))
 
 conf = getconf(conf_dir)
