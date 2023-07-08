63edf49de513af79d123b6ed749778c60ee3bd70
nekorobi_0 <65654766+nekorobi-0@users.noreply.github.com>
2023-07-08 11:30:53 +0900

    いんでんと修正

--- stw.lua
+++ stw.lua
@@ -38,7 +38,7 @@ end
 function split (inputstr, sep)
     local t={}
     for str in string.gmatch(inputstr, "([^"..sep.."]+)") do
-            table.insert(t, str)
+        table.insert(t, str)
     end
     return t
 end
